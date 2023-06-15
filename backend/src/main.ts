5cf97994b121936028418b464abdaefb8a7e1886
yakibuta888 <yakibuta888@gmail.com>
2023-06-15 17:47:28 +0900

    ss7.36,アクセスログの設定

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,15 @@
 import express from 'express'
+import morgan from 'morgan'
 
 const PORT = 3000
 
 const app = express()
 
+app.use(morgan('dev'))
+
 app.get('/api/hello', async (req, res) => {
   res.json({
-    message: 'Hello Express!@!'
+    message: 'Hello Express!!'
   })
 })
 
