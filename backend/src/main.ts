076c5538a0ad15926caac2a4d7f9fad19c445c5d
yakibuta888 <yakibuta888@gmail.com>
2023-06-16 18:02:41 +0900

    ss8.41,対戦画面のCSSの実装

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import morgan from 'morgan'
+import 'express-async-errors'
 
 const PORT = 3000
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(express.static('static', { extensions: ['html'] }))
 
 app.get('/api/hello', async (req, res) => {
   res.json({
@@ -13,6 +15,24 @@ app.get('/api/hello', async (req, res) => {
   })
 })
 
+app.get('/api/error', async(req, res) => {
+  throw new Error('Error endpoint')
+})
+
+app.use(errorHandler)
+
 app.listen(PORT, () => {
   console.log(`Reversi application started: http://localhost:${PORT}`)
 })
+
+function errorHandler(
+  err: any,
+  _req: express.Request,
+  res: express.Response,
+  _next: express.NextFunction
+) {
+  console.error('Unexpected error occurred', err)
+  res.status(500).send({
+    message: 'Unexpected error occurred'
+  })
+}
