ba1f82bd80a444cf160d229866c761ffb107df8a
yakibuta888 <yakibuta888@gmail.com>
2023-06-23 20:22:54 +0900

    ss9.48.対戦を開始するAPIの疎通

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -19,6 +19,13 @@ app.get('/api/error', async(req, res) => {
   throw new Error('Error endpoint')
 })
 
+app.post('/api/games', async (req, res) => {
+  const startedAt = new Date()
+  console.log(`startedAt = ${startedAt}`)
+
+  res.status(201).end()
+})
+
 app.use(errorHandler)
 
 app.listen(PORT, () => {
--- backend/static/main.js
+++ backend/static/main.js
@@ -40,7 +40,14 @@ async function showBoard() {
   })
 }
 
+async function registerGame() {
+  await fetch('/api/games', {
+    method: 'POST'
+  })
+}
+
 async function main() {
+  await registerGame()
   await showBoard()
 }
 
