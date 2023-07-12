e5884331fb45f2ba7a7b7e0d632e4dd1d742018c
yakibuta888 <yakibuta888@gmail.com>
2023-07-12 18:30:42 +0900

    ss13.75.盤面に置けるかチェックする処理の実装

--- backend/src/domain/turn/board.ts
+++ backend/src/domain/turn/board.ts
@@ -1,11 +1,24 @@
 import { Disc } from "./disc";
 import { Move } from "./move";
+import { Point } from "./point";
 
 export class Board {
   constructor(private _discs: Disc[][]) {}
 
   place(move: Move): Board {
     // TODO 盤面に置けるかチェック
+    // 空のマス目ではない場合、置くことはできない
+    if (this._discs[move.point.y][move.point.x] !== Disc.Empty) {
+      throw new Error('Selected point is not empty')
+    }
+
+    // ひっくり返せる点をリストアップ
+    const flipPoints = this.listFlipPoints()
+
+    // ひっくり返せる点が無い場合、置くことはできない
+    if (flipPoints.length === 0) {
+      throw new Error('Flip points is empty')
+    }
 
     // 盤面をコピー
     const newDiscs = this._discs.map((line) => {
@@ -20,6 +33,10 @@ export class Board {
     // TODO ひっくり返す
 
     return new Board(newDiscs)
+  }
+
+  private listFlipPoints(): Point[] {
+    return [new Point(0, 0)]
   }
 
   get discs() {
